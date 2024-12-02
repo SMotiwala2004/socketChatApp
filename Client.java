c825dd16fb2c5e97e2026ef10289bc30f28a693d
Safeer Motiwala <safeer@MacBookPro.attlocal.net>
2024-12-01 22:50:38 -0600

    Client code updated

--- Client.java
+++ Client.java
@@ -1,3 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException{
+        try {
+            // Create Socket
+            Socket s = new Socket("localhost", 9000);
+            System.err.println("Connected to the chat server!");
+
+            // Setup input and output readers
+            PrintWriter out = new PrintWriter(s.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));            
+
+            // Start Thread to handle incoming messages
+            new Thread(() -> {
+                try {
+                    String serverResponse;
+                    while((serverResponse = in.readLine()) != null) {
+                        System.out.println(serverResponse);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            // Read Messages from console to the server
+            Scanner sc = new Scanner(System.in);
+            String userInput;
+            while(true) {
+                userInput = sc.nextLine();
+                out.println(userInput);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
