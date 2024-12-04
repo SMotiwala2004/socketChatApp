cbc2bfb8c30953589efc1ea09da0648eb030449b
Safeer Motiwala <safeer@MacBookPro.attlocal.net>
2024-12-03 22:18:28 -0600

    Setting up ClientHandler, ClientHandler Constructer created

--- Server.java
+++ Server.java
@@ -1,5 +1,8 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -28,6 +31,23 @@ public class Server {
     }
 
     private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private String Username;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+            
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
 
         @Override
         public void run() {
